6422a46b4e275d659e77efe9af84e0da61c26f55
HOANGTHIEN <hoangthien150101@gmail.com>
2022-06-14 16:05:02 +0700

    lab5

---
+++ RubikStore/Controllers/RubikController.cs
@@ -0,0 +1,20 @@
+﻿using RubikStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RubikStore.Controllers
+{
+    public class RubikController : Controller
+    {
+        // GET: Rubik
+        RubikContext data = new RubikContext();
+        public ActionResult Index()
+        {
+            var all_rubik = from s in data.Rubiks select s;
+            return View(all_rubik);
+        }
+    }
+}
---
+++ RubikStore/Models/Giohang.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RubikStore.Models
+{
+    public class Giohang
+    {
+        RubikContext data = new RubikContext();
+        public int id { get; set; }
+        [Display(Name = "Tên")] public string ten { get; set; }
+        [Display(Name = "Ảnh bìa")] public string hinh { get; set; }
+        [Display(Name = "Giá bán")] public double gia { get; set; }
+        [Display(Name = "Số lượng")] public int iSoluong { get; set; }
+        [Display(Name = "Thành tiền")] public Double dThanhtien { get { return iSoluong * gia; } }
+        public Giohang (int id)
+        {
+            this.id = id;
+            Rubik rubik = data.Rubiks.Single(n => n.id == id);
+            ten = rubik.ten;
+            hinh = rubik.hinh;
+            gia = double.Parse(rubik.gia.ToString());
+            iSoluong = 1;
+        }
+    }
+}
